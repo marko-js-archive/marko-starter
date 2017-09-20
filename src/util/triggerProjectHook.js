ecc90bfd2765c9953b6dc7206d95e343ba6e7ebc
Phillip Gates-Idem <phil.gates-idem@lifeomic.com>
2017-09-20 00:22:34 -0400

    Allow hooks to be called with additional args. Add lasso-marko as a dependency. Upgrade lasso. Provide BuildResult to afterBuild hook.

--- src/util/triggerProjectHook.js
+++ src/util/triggerProjectHook.js
@@ -2,14 +2,18 @@
 
 const pluginManager = require('~/src/plugin-manager');
 
-module.exports = (project, hookName) => {
+module.exports = (project, hookName, args) => {
   // Notify plugins first
   let promise = pluginManager.triggerProjectHook(project, hookName);
 
   let hooks = project.getHooks()[hookName];
 
+  if (!args) {
+    args = [project];
+  }
+
   hooks.forEach((hook) => {
-    promise = promise.then(Promise.resolve(hook(project)));
+    promise = promise.then(Promise.resolve(hook.apply(project, args)));
   });
 
   return promise;
